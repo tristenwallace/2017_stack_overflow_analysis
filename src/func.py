65c74b335b9ac2cd096e6b3549028fd4f15d178c
tristenwallace <tri3sten@gmail.com>
2024-01-28 17:09:43 -0500

    feat: List feature importance using coefficients

--- src/func.py
+++ src/func.py
@@ -196,3 +196,25 @@ def find_optimal_feature_cutoff(X, y, cutoffs, plot=False, test_size=.3, rand_st
 
     return best_cutoff, scores_test, scores_train, reg, X_train, X_test, y_train, y_test
 
+
+
+def coef_weights(coefficients, X_train):
+    '''
+    INPUT:
+        coefficients - the coefficients of the linear model 
+        X_train - the training data, so the column names can be used
+    
+    OUTPUT:
+        coefs_df - a dataframe holding the coefficient, estimate, and abs(estimate)
+    
+    Provides a dataframe that can be used to understand the most influential coefficients
+    in a linear model by providing the coefficient estimates along with the name of the 
+    variable attached to the coefficient.
+    '''
+    coefs_df = pd.DataFrame()
+    coefs_df['est_int'] = X_train.columns
+    coefs_df['coefs'] = coefficients
+    coefs_df['abs_coefs'] = np.abs(coefficients)
+    coefs_df = coefs_df.sort_values('abs_coefs', ascending=False)
+    
+    return coefs_df
